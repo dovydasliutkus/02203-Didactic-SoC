232e2a53ebd8abda59452f299d180b38120d97d8
dovydasliutkus <dovli@dtu.dk>
2026-05-11 11:03:25 +0200

    Generate fpga filelists

---
+++ fpga/includes.f
@@ -0,0 +1,8 @@
+../.bender/git/checkouts/common_cells-3bd5b2d671aaec0e/include
+../.bender/git/checkouts/axi-aae2d6c181d6f97b/include
+../.bender/git/checkouts/apb-e4460f86c75afc6a/include
+../.bender/git/checkouts/obi-cbdec09f22f66762/include
+../.bender/git/checkouts/register_interface-26afcadbe8460c5f/include
+../vendor_ips/ibex/vendor/lowrisc_ip/dv/sv/dv_utils
+../vendor_ips/ibex/vendor/lowrisc_ip/ip/prim/rtl
+../vendor_ips/ibex/rtl
